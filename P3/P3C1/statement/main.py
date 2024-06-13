ecbdafaca936daf1b8212e0ab8b2fc72ab414dfe
salmmmaa <benmohamedsalma3@gmail.com>
2024-06-13 10:16:37 +0200

    Update main.py

--- P3/P3C1/statement/main.py
+++ P3/P3C1/statement/main.py
@@ -1,6 +1,8 @@
-# Write your code here!
-
-
-# Do not modify the code below
+import operations
+def main():
+    sum_result = operations.sum(3, 5)
+    print("Result of the sum :", sum_result)
+    product_result = operations.product(8, 2)
+    print("Result of the product :", product_result)
 if __name__ == "__main__":
     main()
