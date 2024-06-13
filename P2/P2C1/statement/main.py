6b430cb5d35e1128d2a51f4123507eab66b75f1b
salmmmaa <benmohamedsalma3@gmail.com>
2024-06-13 04:03:26 +0200

    Update main.py

--- P2/P2C1/statement/main.py
+++ P2/P2C1/statement/main.py
@@ -1,8 +1,29 @@
 def main():
-    # Write your code here!
-    # Be careful, all your code must be indented like this comment !
+    left_number = input("Enter an integer number : ")
+    right_number = input("Enter an integer number : ")
+    operation = input("Enter the desired operation ['+', '-', '*', or '/'] : ")
+    result = 0
+    if not left_number.isnumeric() or not right_number.isnumeric():
+        print("Error: both numbers must be integers")
+    else:
+        left_number = int(left_number)
+        right_number = int(right_number)
+        match operation:
+            case "+":
+                result = left_number + right_number
+            case "-":
+                result = left_number - right_number
+            case "*":
+                result = left_number * right_number
+            case "/":
+                if right_number == 0:
+                    print("Error: division by zero is not allowed.")
+                else:
+                    result = left_number / right_number
+            case _:
+                print("Error: the operation symbol must be '+', '-', '*', or '/'.")
 
+        print(f"The result of the operation is: {result}")
 
-# Do not modify the code below
 if __name__ == "__main__":
     main()
