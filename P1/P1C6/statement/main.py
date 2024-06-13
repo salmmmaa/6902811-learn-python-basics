b263e1fed1e6cf2524dafc686710eb4d7559dd49
salmmmaa <benmohamedsalma3@gmail.com>
2024-06-13 03:16:19 +0200

    Update main.py

--- P1/P1C6/statement/main.py
+++ P1/P1C6/statement/main.py
@@ -1,1 +1,7 @@
-# Write your code here!
+fruits = ["apple", "banana", "orange"]
+fruits.append("kiwi")
+fruits.remove("orange")
+fruits[1] = "pineapple"
+print("The fruits list contains", len(fruits), "elements.")
+fruits.sort()
+print(fruits)
