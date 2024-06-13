83eb966052976e815f741920963836bacbdec4c3
salmmmaa <benmohamedsalma3@gmail.com>
2024-06-13 03:34:17 +0200

    Update main.py

--- P1/P1C7/statement/main.py
+++ P1/P1C7/statement/main.py
@@ -1,1 +1,5 @@
-# Write your code here!
+fruits = {"apple": "red", "banana": "yellow", "orange": "orange"} 
+fruits["kiwi"] = "green"
+color_banana = fruits["banana"] 
+del fruits["banana"] 
+print(fruits.keys())
