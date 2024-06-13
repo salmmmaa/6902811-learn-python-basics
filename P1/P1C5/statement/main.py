d30ae31b3a95c9cdb471e1241c0185d5d003c159
salmmmaa <benmohamedsalma3@gmail.com>
2024-06-13 03:09:27 +0200

    Update main.py

--- P1/P1C5/statement/main.py
+++ P1/P1C5/statement/main.py
@@ -1,1 +1,12 @@
-# Write your code here!
+name = "salma"
+age = 18
+height = 1.70
+is_student = True
+print(f"name: {name}")
+print(f"age: {age}")
+print(f"height: {height}")
+print(f"is student: {is_student}")
+print(f"Type name: {type(name)}")
+print(f"Type age: {type(age)}")
+print(f"Type height: {type(height)}")
+print(f"Type is student: {type(is_student)}")
