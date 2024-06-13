cb5aa0d8bc8a4aebc10143c9480ecbd05bf7eb37
salmmmaa <benmohamedsalma3@gmail.com>
2024-06-13 04:15:46 +0200

    Update main.py

--- P2/P2C3/statement/main.py
+++ P2/P2C3/statement/main.py
@@ -1,5 +1,19 @@
-# Write your code here!
+def monthly_salary(annual_salary):
+    return annual_salary / 12
+def weekly_salary(monthly_salary):
+    return monthly_salary / 4
+def hourly_wage(weekly_salary, hours_worked):
+    return weekly_salary / hours_worked
 
-# Do not modify the code below
+
+def main():
+    annual_salary = float(input("Enter your annual salary : "))
+    hours_worked = float(input("Enter the number of hours worked per week : "))
+    monthly = monthly_salary(annual_salary)
+    weekly = weekly_salary(monthly)
+    hourly = hourly_wage(weekly, hours_worked)
+
+    print("Your hourly wage is",  hourly, "euros.")
+
 if __name__ == "__main__":
     main()
